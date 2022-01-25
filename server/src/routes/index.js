bc2f838b28e4f1ed5e2a8d4fc5e673907949be37
Jody Septiawan <jody@Jodys-MacBook-Air.local>
2022-01-25 13:56:19 +0700

    update

--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -1,4 +1,4 @@
-const express = require("express");
+const express = require('express');
 
 const router = express.Router();
 
@@ -9,58 +9,58 @@ const {
   getUser,
   updateUser,
   deleteUser,
-} = require("../controllers/user");
+} = require('../controllers/user');
 const {
   getProducts,
   getProduct,
   addProduct,
   updateProduct,
   deleteProduct,
-} = require("../controllers/product");
+} = require('../controllers/product');
 const {
   getTransactions,
   addTransaction,
-} = require("../controllers/transaction");
+} = require('../controllers/transaction');
 const {
   getCategories,
   addCategory,
   updateCategory,
   getCategory,
   deleteCategory,
-} = require("../controllers/category");
-const { getProfile } = require("../controllers/profile");
-const { register, login, checkAuth } = require("../controllers/auth");
+} = require('../controllers/category');
+const { getProfile } = require('../controllers/profile');
+const { register, login, checkAuth } = require('../controllers/auth');
 
 // Middleware
-const { auth } = require("../middlewares/auth");
-const { uploadFile } = require("../middlewares/uploadFile");
+const { auth } = require('../middlewares/auth');
+const { uploadFile } = require('../middlewares/uploadFile');
 
 // Route
-router.post("/user", addUsers);
-router.get("/users", getUsers);
-router.get("/user/:id", getUser);
-router.delete("/user/:id", updateUser);
-router.delete("/user/:id", deleteUser);
+router.post('/user', addUsers);
+router.get('/users', getUsers);
+router.get('/user/:id', getUser);
+router.delete('/user/:id', updateUser);
+router.delete('/user/:id', deleteUser);
 
-router.get("/profile", auth, getProfile);
+router.get('/profile', auth, getProfile);
 
-router.get("/products", auth, getProducts);
-router.get("/product/:id", auth, getProduct);
-router.post("/product", auth, uploadFile("image"), addProduct);
-router.patch("/product/:id", auth, uploadFile("image"), updateProduct);
-router.delete("/product/:id", auth, deleteProduct);
+router.get('/products', getProducts);
+router.get('/product/:id', auth, getProduct);
+router.post('/product', auth, uploadFile('image'), addProduct);
+router.patch('/product/:id', auth, uploadFile('image'), updateProduct);
+router.delete('/product/:id', auth, deleteProduct);
 
-router.get("/transactions", auth, getTransactions);
-router.post("/transaction", auth, addTransaction);
+router.get('/transactions', auth, getTransactions);
+router.post('/transaction', auth, addTransaction);
 
-router.get("/categories", getCategories);
-router.get("/category/:id", getCategory);
-router.post("/category", addCategory);
-router.patch("/category/:id", updateCategory);
-router.delete("/category/:id", deleteCategory);
+router.get('/categories', getCategories);
+router.get('/category/:id', getCategory);
+router.post('/category', addCategory);
+router.patch('/category/:id', updateCategory);
+router.delete('/category/:id', deleteCategory);
 
-router.post("/register", register);
-router.post("/login", login);
-router.get("/check-auth", auth, checkAuth);
+router.post('/register', register);
+router.post('/login', login);
+router.get('/check-auth', auth, checkAuth);
 
 module.exports = router;
