6c6b819e8f1166ccc2b4b3cfe68c5209a3d47565
Jody Septiawan <jody@Jodys-MacBook-Air.local>
2022-01-25 15:59:43 +0700

    update

--- client/src/pages/CategoryAdmin.js
+++ client/src/pages/CategoryAdmin.js
@@ -1,32 +1,38 @@
-import React, { useState, useEffect } from "react";
-import { Container, Row, Col, Table, Button } from "react-bootstrap";
-import { useHistory } from "react-router";
+import React, { useState, useEffect } from 'react';
+import { Container, Row, Col, Table, Button } from 'react-bootstrap';
+import { useHistory } from 'react-router';
 
-import NavbarAdmin from "../components/NavbarAdmin";
-import DeleteData from "../components/modal/DeleteData";
+import NavbarAdmin from '../components/NavbarAdmin';
+import DeleteData from '../components/modal/DeleteData';
 
-import dataCategory from "../fakeData/category";
-import imgEmpty from "../assets/empty.svg";
+import dataCategory from '../fakeData/category';
+import imgEmpty from '../assets/empty.svg';
 
-// Get API config here ...
+// API config
+import { API } from '../config/api';
 
 export default function CategoryAdmin() {
   let history = useHistory();
 
-  const title = "Category admin";
-  document.title = "DumbMerch | " + title;
+  const title = 'Category admin';
+  document.title = 'DumbMerch | ' + title;
 
   // Variabel for store category data
   const [categories, setCategories] = useState([]);
 
-  // Create variabel for id category and confirm delete data with useState here ...
+  // Variabel for delete category data
+  const [idDelete, setIdDelete] = useState(null);
+  const [confirmDelete, setConfirmDelete] = useState(null);
 
-  // Init useState & function for handle show-hide modal confirm here ...
+  // Modal Confirm delete data
+  const [show, setShow] = useState(false);
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
 
   // Fetching categories data from database
   const getCategories = async () => {
     try {
-      const response = await API.get("/categories");
+      const response = await API.get('/categories');
       // Store categories data to useState variabel
       setCategories(response.data.data);
     } catch (error) {
@@ -42,15 +48,36 @@ export default function CategoryAdmin() {
     history.push(`update-category/${id}`);
   };
 
-  // Create function handle get id category & show modal confirm delete data here ...
+  // For get id category & show modal confirm delete data
+  const handleDelete = (id) => {
+    setIdDelete(id);
+    handleShow();
+  };
 
   /// Create function for handle delete category here ...
   // If confirm is true, execute delete data
+  const deleteById = async (id) => {
+    try {
+      await API.delete(`/category/${id}`);
+      getCategories();
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
   // Call function for handle close modal and execute delete data with useEffect here ...
+  useEffect(() => {
+    if (confirmDelete) {
+      // Close modal confirm delete data
+      handleClose();
+      // execute delete data by id function
+      deleteById(idDelete);
+      setConfirmDelete(null);
+    }
+  }, [confirmDelete]);
 
   const addCategory = () => {
-    history.push("/add-category");
+    history.push('/add-category');
   };
 
   return (
@@ -63,7 +90,11 @@ export default function CategoryAdmin() {
             <div className="text-header-category mb-4">List Category</div>
           </Col>
           <Col className="text-end">
-            <Button onClick={addCategory} className="btn-dark" style={{ width: "100px" }}>
+            <Button
+              onClick={addCategory}
+              className="btn-dark"
+              style={{ width: '100px' }}
+            >
               Add
             </Button>
           </Col>
@@ -92,7 +123,7 @@ export default function CategoryAdmin() {
                             handleEdit(item.id);
                           }}
                           className="btn-sm btn-success me-2"
-                          style={{ width: "135px" }}
+                          style={{ width: '135px' }}
                         >
                           Edit
                         </Button>
@@ -101,7 +132,7 @@ export default function CategoryAdmin() {
                             handleDelete(item.id);
                           }}
                           className="btn-sm btn-danger"
-                          style={{ width: "135px" }}
+                          style={{ width: '135px' }}
                         >
                           Delete
                         </Button>
@@ -112,14 +143,23 @@ export default function CategoryAdmin() {
               </Table>
             ) : (
               <div className="text-center pt-5">
-                <img src={imgEmpty} className="img-fluid" style={{ width: "40%" }} alt="empty" />
+                <img
+                  src={imgEmpty}
+                  className="img-fluid"
+                  style={{ width: '40%' }}
+                  alt="empty"
+                />
                 <div className="mt-3">No data category</div>
               </div>
             )}
           </Col>
         </Row>
       </Container>
-      <DeleteData setConfirmDelete={setConfirmDelete} show={show} handleClose={handleClose} />
+      <DeleteData
+        setConfirmDelete={setConfirmDelete}
+        show={show}
+        handleClose={handleClose}
+      />
     </>
   );
 }
